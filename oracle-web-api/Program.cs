e24f46e397eacb32c9013462f32c745bcac36536
Web HM <133254421+webhm@users.noreply.github.com>
2025-01-17 11:53:33 -0500

    Update Program.cs

--- oracle-web-api/Program.cs
+++ oracle-web-api/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddDbContext<ModelContext>(x =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
